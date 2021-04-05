90cb9dbff55827141c2d24c66d19aaa29b8a7348
nickvinok <nickvinok@yandex.ru>
2021-04-05 09:00:12 +0300

    1

---
+++ src/main/java/com/diploma/Diploma/Controllers/DatabaseInteractionForm/ClutterClassController.java
@@ -0,0 +1,49 @@
+package com.diploma.Diploma.Controllers.DatabaseInteractionForm;
+
+import com.diploma.Diploma.DataBase.Model.ClutterClass;
+import com.diploma.Diploma.DataBase.Model.Event;
+import com.diploma.Diploma.DataBase.Repo.ClutterClassRepo;
+import com.diploma.Diploma.DataBase.Repo.EventRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/clutterClass")
+public class ClutterClassController {
+    @Autowired
+    ClutterClassRepo repo;
+
+    @PostMapping("/get")
+    public Optional<ClutterClass> getEvent(@RequestBody ClutterClass c){
+        return repo.findById(c.getId());
+    }
+
+    @GetMapping()
+    public List<ClutterClass> getEvent(){
+        return repo.findAll();
+    }
+
+    @PostMapping("/create")
+    public ClutterClass newEvent(@RequestBody ClutterClass event){
+        return repo.save(event);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<ClutterClass> updateEvent(@RequestBody ClutterClass event){
+        Optional<ClutterClass> tmp = repo.findById(event.getId());
+        if(tmp.isPresent()){
+            return ResponseEntity.ok(repo.save(event));
+        } else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/delete")
+    public void deleteEvent(@RequestBody ClutterClass event){
+        repo.deleteById(event.getId());
+    }
+}
---
+++ src/main/java/com/diploma/Diploma/Controllers/DatabaseInteractionForm/ExplosionSensitivityController.java
@@ -0,0 +1,49 @@
+package com.diploma.Diploma.Controllers.DatabaseInteractionForm;
+
+import com.diploma.Diploma.DataBase.Model.Event;
+import com.diploma.Diploma.DataBase.Model.ExplosionSensitivity;
+import com.diploma.Diploma.DataBase.Repo.EventRepo;
+import com.diploma.Diploma.DataBase.Repo.ExplosionSensitivityRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/explosionSensitivity")
+public class ExplosionSensitivityController {
+    @Autowired
+    ExplosionSensitivityRepo repo;
+
+    @PostMapping("/get")
+    public Optional<ExplosionSensitivity> getEvent(@RequestBody ExplosionSensitivity event){
+        return repo.findById(event.getId());
+    }
+
+    @GetMapping()
+    public List<ExplosionSensitivity> getEvent(){
+        return repo.findAll();
+    }
+
+    @PostMapping("/create")
+    public ExplosionSensitivity newEvent(@RequestBody ExplosionSensitivity event){
+        return repo.save(event);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<ExplosionSensitivity> updateEvent(@RequestBody ExplosionSensitivity event){
+        Optional<ExplosionSensitivity> tmp = repo.findById(event.getId());
+        if(tmp.isPresent()){
+            return ResponseEntity.ok(repo.save(event));
+        } else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/delete")
+    public void deleteEvent(@RequestBody ExplosionSensitivity event){
+        repo.deleteById(event.getId());
+    }
+}
