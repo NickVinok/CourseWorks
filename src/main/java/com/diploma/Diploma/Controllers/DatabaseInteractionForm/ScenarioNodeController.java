4d8379ed71aa07dde648de6ea91d2892e5d67599
nickvinok <nickvinok@yandex.ru>
2021-05-23 16:06:41 +0300

    fixed scenarioNode Controller

--- src/main/java/com/diploma/Diploma/Controllers/DatabaseInteractionForm/ScenarioNodeController.java
+++ src/main/java/com/diploma/Diploma/Controllers/DatabaseInteractionForm/ScenarioNodeController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/scenarioNode")
+@RequestMapping("/emergencyScenarioNode")
 public class ScenarioNodeController {
     @Autowired
     ScenarioNodeRepo repo;
